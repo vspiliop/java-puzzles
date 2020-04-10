1911e909990d66fa5cb1096b5c5931b1b18f6714
Vassilis Spiliopoulos <2billy@gmail.com>
2020-04-10 14:26:18 +0100

    refactor mirror string to not use for loops

--- src/gy/etiolo/puzzles/streams/MirrorStrings.java
+++ src/gy/etiolo/puzzles/streams/MirrorStrings.java
@@ -1,6 +1,9 @@
 package gy.etiolo.puzzles.streams;
 
 import java.util.Stack;
+import java.util.stream.IntStream;
+
+import static java.util.stream.IntStream.iterate;
 
 /**
  * Copyright (c)2020 DFS Services LLC
@@ -9,29 +12,23 @@ import java.util.Stack;
  * @author vspilio
  */
 public class MirrorStrings {
-  public static void main(String... args) {
-    String word = "abccba";
-    System.out.println(isMirror(word));
-  }
-
-  private static boolean isMirror(String word) {
-    if (word.length() % 2 != 0)
-      return false;
-
-    Stack<Character> stack = new Stack();
-
-    char[] charArray = word.toCharArray();
-
-    for(int i = 0; i < charArray.length/2; i++) {
-      stack.push(charArray[i]);
+    public static void main(String... args) {
+        String word = "abccba";
+        System.out.println(isMirror(word));
     }
 
-    for (int i = charArray.length/2; i < charArray.length; i++) {
-      if (charArray[i] != stack.pop())
-        return false;
-    }
+    private static boolean isMirror(String word) {
+        if (word.length() % 2 != 0)
+            return false;
 
-    return true;
-  }
+        Stack<Character> stack = new Stack();
+
+        char[] charArray = word.toCharArray();
+
+        IntStream.range(0, charArray.length / 2).forEach(i -> stack.push(charArray[i]));
+
+        return iterate(charArray.length / 2, i -> i < charArray.length , i -> i + 1)
+                            .allMatch(i -> charArray[i] == stack.pop());
+    }
 
 }
